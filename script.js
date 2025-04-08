5ff97095fc77a6301558eeef8da2240df4a35765
Bhawana <103374144+bhawana1107@users.noreply.github.com>
2025-04-08 18:37:59 +0000

    change

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-function handleClick(el){
+function handleClick(el) {
     let bill = parseFloat(document.getElementById('bill').value);
     let tip = parseFloat(document.getElementById('tip').value);
 
-    let percent = (bill/100) * tip;
-    let result = bill + percent ;
-    
+    let percent = (bill / 100) * tip;
+    let result = bill + percent;
+
     document.getElementById('total').innerText = result.toFixed(2);
     document.getElementById('bill').disabled = true;
     document.getElementById('tip').disabled = true;
-    
+
+    document.getElementById('reset').style.display = "block";
 }
